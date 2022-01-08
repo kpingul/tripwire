839089c222d18ec6820366f0df3cb10db274b496
Joel Pingul <kirckpingul@gmail.com>
2022-01-08 14:15:44 -0800

    now checking if file has been accessed

--- tripwire.go
+++ tripwire.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"time"
   	"github.com/djherbis/times"
   	"github.com/jasonlvhit/gocron"
 )
 
+
+//global variables
 var (
 	lastAccessTime time.Time
 )
 
 func main() {
+	lastAccessTime = time.Now()
+
 	gocron.Every(10).Second().Do(checkFileChanges)
 
 	// Start all the pending jobs
@@ -19,12 +23,19 @@ func main() {
 }
 
 func checkFileChanges() {
-	t, err := times.Stat("./test.txt")
+
+	fileStat, err := times.Stat("./test.txt")
   	if err != nil {
-    		log.Fatal(err.Error())
+    		fmt.Println(err.Error())
   	}
 
-  	log.Println(t.AccessTime())
- 	log.Println(t.ModTime())
+  	//checks if last access time changed
+  	//then updates the last access time to 
+  	//current access time for next check
+    	if fileStat.AccessTime().After(lastAccessTime) {
+    		fmt.Println("File Access")
+    		lastAccessTime = fileStat.AccessTime()
+    	}
+
 
 }
